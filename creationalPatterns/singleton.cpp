22c9b9e13ba6f00f00eac78f52db548adb70b5d8
ludan <ludanlu@lips-hci.com>
2018-07-12 17:51:06 +0800

    update

--- creationalPatterns/singleton.cpp
+++ creationalPatterns/singleton.cpp
@@ -6,16 +6,16 @@ class Singleton
 {
         private:
 		int num;
+                Singleton() = default;
                 Singleton(const Singleton&) = delete;
                 Singleton& operator=(const Singleton&) = delete;
 
                 static std::unique_ptr<Singleton> object;
         public:
-                Singleton() = default;
 
                 static Singleton& Instance()
                 {
-                        object.reset(new Singleton); 
+                        if(!object) object.reset(new Singleton); 
                         std::cout << "Getting  Singleton object\n";
                         return *(object.get());
                 }
@@ -23,16 +23,16 @@ class Singleton
 		void setNum(int i) { object->num = i; }
 };
 
-std::unique_ptr<Singleton> Singleton::object;
+std::unique_ptr<Singleton> Singleton::object = nullptr;
 
 int main()
 {
         Singleton& s1 = Singleton::Instance();
-        std::cout << s1.getNum() << std::endl;
+        std::cout << "s1 num: " << s1.getNum() << std::endl;
         s1.setNum(2);
-        std::cout << s1.getNum() << std::endl;
+        std::cout << "s1 num: " << s1.getNum() << std::endl;
         Singleton& s2 = Singleton::Instance();
-        std::cout << s2.getNum() << std::endl;
+        std::cout << "s2 num: " << s2.getNum() << std::endl;
 
         return 0;
 }
